0525a89375910c6361276afd263e816dbe619219
Anton Shpakovych <shpakovych18@gmail.com>
2023-10-18 16:41:29 +0300

    Added models: Employee, Position, Command, Project

--- command_manager/models.py
+++ command_manager/models.py
@@ -1,1 +1,60 @@
 from django.db import models
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+
+class Employee(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    experience = models.FloatField()
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="employees"
+    )
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Command(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    employees = models.ManyToManyField(Employee, related_name="commands")
+
+    @property
+    def command_size(self) -> int:
+        return self.employees.objects.count()
+
+    @property
+    def command_experience(self) -> float:
+        return sum(
+            employee.experience
+            for employee in self.employees.objects.all()
+        )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    budget = models.DecimalField(max_digits=9, decimal_places=2)
+    commands = models.ManyToManyField(Command, related_name="projects")
+    deadline = models.DateField()
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
